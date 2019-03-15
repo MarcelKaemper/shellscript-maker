d81119b7fabbe5a95ce77b7e0dc9c5ea1027dd43
Marcel <marcelkaemper862@gmail.com>
2019-03-15 16:13:23 +0100

    Added file append function

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import os
 
 print("Welcome to the shellscript construction kit\n")
 
-filename=input("Enter the name of your file >> ")
-os.system("touch trash/"+filename+".sh")
+def append(filename, string):
+    with open(filename,"a") as w:
+        w.write(string)
+
+path="trash/"+input("Enter the name of your file >> ")+".sh"
+
+os.system("touch "+path)
+append(path, "Howdy\n")
 
 operation = ["Echo"]
 
 while(1):
     print("Choose the module you want to add:\n")
-    for i, elmnt in enumerate(operation):
+    for i, elmnt in enumerate(operation,start=1):
         print("["+str(i)+"] "+elmnt+"\n")
     input()
