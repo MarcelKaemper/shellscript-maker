b2cb9e66eafad31c4632e5bf7f3260897ac426e4
Marcel <marcelkaemper862@gmail.com>
2019-03-16 13:21:16 +0100

    Changed function calling; Added variable function

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ print("Welcome to the shellscript construction kit\n")
 
 def append(filename, string):
     with open(filename,"a") as w:
-        w.write(string)
+        w.write(string+"\n")
+
+def echo():
+    append(path, "echo "+input("Text to echo: "))
+
+def variable():
+    name = input("Name:")
+    value = input("Value:")
+    if ' ' in value:
+        value="\""+value+"\""
+    append(path, name+"="+value)
 
 path=input("Enter the name of your file >> ")+".sh"
 
@@ -12,14 +22,12 @@ os.system("touch "+path)
 os.system("chmod +x "+path)
 append(path, "#!bin/shell\n")
 
-modules = ["Exit", "Echo"]
+modules = ["Echo", "Variable", "If/Else", "Exit"]
 
 while(1):
     print("Choose the module you want to add:\n")
-    for i, elmnt in enumerate(modules,start=0):
+    for i, elmnt in enumerate(modules,start=1):
         print("["+str(i)+"] "+elmnt+"\n")
-    module = input("Module:")
-    if(module == "0"):
-        exit()
-    elif(module == "1"):
-        append(path, "echo "+input("Text to echo:"))
+
+    module = input("Choose module: ")
+    eval(modules[int(module)-1].lower()+"()")
