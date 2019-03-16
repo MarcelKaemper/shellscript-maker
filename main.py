43727f83a619e107aa883b9aa471904686df2db9
Marcel <marcelkaemper862@gmail.com>
2019-03-16 15:16:20 +0100

    Added variable finding function; Multiple choice menu on var echo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 print("Welcome to the shellscript construction kit\n")
 
@@ -7,11 +8,14 @@ def append(filename, string):
         w.write(string+"\n")
 
 def echo():
-    choice = input("[1] Echo text\n[2] Echo variable\n>>")
+    choice = input("[1] Echo text\n[2] Echo variable\n>> ")
     if(choice == "1"):
         append(path, "echo "+input("Text to echo: "))
     elif(choice == "2"):
-        append(path, "echo $"+input("Variable name: "))
+        variables = findVar(path)
+        for i in range(len(variables[0])):
+            print("["+str(i)+"] "+variables[0][i])
+        input("Choose a variable\n>> ") 
 
 
 def variable():
@@ -21,6 +25,17 @@ def variable():
         value="\""+value+"\""
     append(path, name+"="+value)
 
+def findVar(filename):
+    variables = [[],[]]
+    pattern=r'^(\S+) *[=] *"*([^"\n]+)"*;*[\n]*$'
+    with open(filename,"r") as w:
+        for element in w.readlines():
+            obj = re.match(pattern, element)
+            if obj != None:
+                variables[0].append(obj.group(1))
+                variables[1].append(obj.group(2))
+    return variables
+
 path=input("Enter the name of your file >> ")+".sh"
 
 os.system("touch "+path)
@@ -28,6 +43,7 @@ os.system("chmod +x "+path)
 append(path, "#!bin/shell\n")
 
 modules = ["Echo", "Variable", "If/Else", "Exit"]
+
 
 while(1):
     print("Choose the module you want to add:\n")
